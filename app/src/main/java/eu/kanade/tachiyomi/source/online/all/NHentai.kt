2fc44987ff3bdff1b2bca313aba0830ab1753992
az4521 <cheeze.sprinkels@gmail.com>
2019-03-19 22:16:47 -0400

    yeet ourselves around the rate limit

--- app/src/main/java/eu/kanade/tachiyomi/source/online/all/NHentai.kt
+++ app/src/main/java/eu/kanade/tachiyomi/source/online/all/NHentai.kt
@@ -44,7 +44,7 @@ class NHentai(context: Context) : ParsedHttpSource() {
 
     override fun headersBuilder(): Headers.Builder {
 	return Headers.Builder()
-		.add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.92 Safari/537.36")
+		.add("User-Agent", "Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36")
     }
 
     private val searchUrl = "$baseUrl/search"
